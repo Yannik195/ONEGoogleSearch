b650606e287b606652f346cf6e65940ef34e2b95
ys024 <ys024@hdm-stuttgart.de>
2019-08-26 15:07:15 +0200

    JSON Parsing Test Implementation

--- app/src/main/java/de/yanniksimon/onegooglesearch/MainActivity.java
+++ app/src/main/java/de/yanniksimon/onegooglesearch/MainActivity.java
@@ -2,6 +2,19 @@ package de.yanniksimon.onegooglesearch;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +22,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getJsonArray();
+    }
+
+    private void getJsonArray() {
+        final String url = "https://jsonplaceholder.typicode.com/users";
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        for (int i = 0; i < response.length(); i++){
+
+                            try {
+                                JSONObject user = response.getJSONObject(i);
+                                Log.i("JsonResponse", user.getString("email"));
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
+        requestQueue.add(jsonArrayRequest);
     }
 }
